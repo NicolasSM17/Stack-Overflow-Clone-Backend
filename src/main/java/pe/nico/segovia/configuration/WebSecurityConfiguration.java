137f6d4ff7b6916538af42347558f34e0e3dff1d
NicolasSM17 <segoviamatosjesunicolas@gmail.com>
2024-01-09 19:32:51 -0500

    agregando filtro de solicitud JWT

--- src/main/java/pe/nico/segovia/configuration/WebSecurityConfiguration.java
+++ src/main/java/pe/nico/segovia/configuration/WebSecurityConfiguration.java
@@ -1,5 +1,6 @@
 package pe.nico.segovia.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -11,11 +12,16 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import pe.nico.segovia.filter.JwtRequestFilter;
 
 @Configuration
 @EnableMethodSecurity
 @EnableWebSecurity
 public class WebSecurityConfiguration {
+
+    @Autowired
+    private JwtRequestFilter jwtRequestFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
@@ -28,6 +34,7 @@ public class WebSecurityConfiguration {
                 .sessionManagement(session -> {
                     session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
                 })
+                .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
